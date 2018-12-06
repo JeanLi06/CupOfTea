ee306e465d2f9f53a1cecb2dbd3416f51b2899cd
JeanLi06 <jliautaud@free.fr>
2018-12-06 18:22:12 +0100

    First commit

---
+++ header.php
@@ -0,0 +1,47 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8" />
+		<title>Module 12 Cup of tea</title>
+		<!-- Appel CSS -->
+		<link href="https://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet">
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+		<link rel="stylesheet" href="css/normalize.css">
+		<meta name="viewport" content="width=device-width, user-scalable=no">
+		<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+		<link rel="stylesheet" href="css/flexslider.css" type="text/css">
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+		<script src="js/jquery.flexslider.js"></script>
+		<script charset="utf-8" type="text/javascript">
+		$(window).load(function() {
+			$('.flexslider').flexslider();
+			}
+		);
+		
+		$(".flexslider").flexslider({ directionNav: false });
+		</script>
+		<link rel="stylesheet" href="css/style.css">
+		<!-- SLIDER -->
+	</head>
+	<body>
+		<!--HEADER-->
+		<header>
+			<img src="img/ribbon.svg">
+			<section id="sectionHeader1">
+				<p>Livraison offerte à partir de 65€ d'achat</p>
+			</section>
+			<div class="enTete">
+				<section id="sectionHeader2">
+					<a href="index.php"><img src="img/logo.png" alt="logo" id="logo"></a>
+					<section id="sectionHeader3"><span id="panier">Mon Panier </span><span><i class="fas fa-shopping-cart"></i> 42,00€</span>
+					</section>
+			</section>
+			<nav>
+				<a href="thes.php">thés</a>
+				<a href="">grands crus</a>
+				<a href="">accessoires</a>
+				<a href="">épicerie</a>
+				<a href="">notre histoire</a>
+			</nav>
+		</div>
+	</header>
---
+++ thes.php
@@ -0,0 +1,143 @@
+<?php include("header.php"); ?>
+<main  class="container">
+	<section class = "listingThes">
+		<article>
+			<img src="img/tea/1.jpg" alt="">
+			<h2>Thé noir</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis eum illo corporis nemo, repudiandae sed libero vero natus labore autem sequi alias at similique mollitia totam eius sint incidunt debitis aliquam iure quas doloribus cumque distinctio quae! Magni a eligendi placeat distinctio deserunt culpa beatae officiis, excepturi quos id quia illum fugiat.</p>
+		</article>
+		<section class = "listingThes3thes">
+			<article>
+				<h3>Blue of London</h3>
+				<img src="img/product/product1.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+			<article>
+				<h3>Thé des Lords</h3>
+				<img src="img/product/product2.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+			<article>
+				<h3>Thé des vahinés</h3>
+				<img src="img/product/product3.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+		</section>
+	</section>
+
+	<section class = "listingThes">
+		<article>
+			<img src="img/tea/2.jpg" alt="">
+			<h2>Thé vert</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis eum illo corporis nemo, repudiandae sed libero vero natus labore autem sequi alias at similique mollitia totam eius sint incidunt debitis aliquam iure quas doloribus cumque distinctio quae! Magni a eligendi placeat distinctio deserunt culpa beatae officiis, excepturi quos id quia illum fugiat.</p>
+		</article>
+		<section class = "listingThes3thes">
+			<article>
+				<h3>Vive les fêtes</h3>
+				<img src="img/product/product1.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+			<article>
+				<h3>Fleur d'orange Olong</h3>
+				<img src="img/product/product2.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+			<article>
+				<h3>Olong 7 agrumes</h3>
+				<img src="img/product/product3.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+		</section>
+	</section>
+
+	<section class = "listingThes">
+		<article>
+			<img src="img/tea/3.jpg" alt="">
+			<h2>Oolong</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis eum illo corporis nemo, repudiandae sed libero vero natus labore autem sequi alias at similique mollitia totam eius sint incidunt debitis aliquam iure quas doloribus cumque distinctio quae! Magni a eligendi placeat distinctio deserunt culpa beatae officiis, excepturi quos id quia illum fugiat.</p>
+		</article>
+		<section class = "listingThes3thes">
+			<article>
+				<h3>Blue of London</h3>
+				<img src="img/product/product1.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+			<article>
+				<h3>Thé des Lords</h3>
+				<img src="img/product/product2.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+			<article>
+				<h3>Thé des vahinés</h3>
+				<img src="img/product/product3.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+		</section>
+	</section>
+
+	<section class = "listingThes">
+		<article>
+			<img src="img/tea/4.jpg" alt="">
+			<h2>Thé blanc</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis eum illo corporis nemo, repudiandae sed libero vero natus labore autem sequi alias at similique mollitia totam eius sint incidunt debitis aliquam iure quas doloribus cumque distinctio quae! Magni a eligendi placeat distinctio deserunt culpa beatae officiis, excepturi quos id quia illum fugiat.</p>
+		</article>
+		<section class = "listingThes3thes">
+			<article>
+				<h3>Blue of London</h3>
+				<img src="img/product/product1.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+			<article>
+				<h3>Thé des Lords</h3>
+				<img src="img/product/product2.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+			<article>
+				<h3>Thé des vahinés</h3>
+				<img src="img/product/product3.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+		</section>
+	</section>
+
+	<section class = "listingThes">
+		<article>
+			<img src="img/tea/5.jpg" alt="">
+			<h2>Rooibos</h2>
+			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quis eum illo corporis nemo, repudiandae sed libero vero natus labore autem sequi alias at similique mollitia totam eius sint incidunt debitis aliquam iure quas doloribus cumque distinctio quae! Magni a eligendi placeat distinctio deserunt culpa beatae officiis, excepturi quos id quia illum fugiat.</p>
+		</article>
+		<section class = "listingThes3thes">
+			<article>
+				<h3>Blue of London</h3>
+				<img src="img/product/product1.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+			<article>
+				<h3>Thé des Lords</h3>
+				<img src="img/product/product2.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+			<article>
+				<h3>Thé des vahinés</h3>
+				<img src="img/product/product3.jpg" alt="">
+				<p>A partir de</p><strong class="prix">8,50€</strong>
+				<a href="">voir ce produit</a>
+			</article>
+		</section>
+	</section>
+</main>
+<?php include("footer.php"); ?>
