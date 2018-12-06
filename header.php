66829a3249b529ef22a3284f6cc4a2b54c21600c
JeanLi06 <jliautaud@free.fr>
2018-12-06 22:53:18 +0100

    push

--- header.php
+++ header.php
@@ -7,8 +7,7 @@
 		<link href="https://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet">
 		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
 		<link rel="stylesheet" href="css/normalize.css">
-		<meta name="viewport" content="width=device-width, user-scalable=no">
-		<meta name="viewport" content="initial-scale=1, maximum-scale=1">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
 		<link rel="stylesheet" href="css/flexslider.css" type="text/css">
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
 		<script src="js/jquery.flexslider.js"></script>
--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 
 	<main  class="container">
 		<h1>C'est noël, profitez-en !</h1>
-		<img src="img/offre-noel.jpg" alt="offre-noel" id="bandeauPromotions">
+		<img src="img/offre-noel.jpg" alt="offre-noel" title="offre-noel" id="bandeauPromotions">
 		<h3>Pour toute commande effectuée avant le 20 décembre</h3>
 		<section class="container">
 		<!-- SLIDER -->
@@ -17,16 +17,16 @@
 	<!-- 5 thés -->
 	<section id="cinqThes">
 		<h2>Choisissez votre thé</h2>
-		<a href="#"><img src="img/tea/1.jpg" alt=""><h3>Thé noir</h3></a>
-		<a href="#"><img src="img/tea/2.jpg" alt=""><h3>Thé vert</h3></a>
-		<a href="#"><img src="img/tea/3.jpg" alt=""><h3>Oolong</h3></a>
-		<a href="#"><img src="img/tea/4.jpg" alt=""><h3>Thé blanc</h3></a>
-		<a href="#"><img src="img/tea/5.jpg" alt=""><h3>Rooibos</h3></a>
+		<a href="#"><img src="img/tea/1.jpg" alt="Thé noir" title="Thé noir"><h3>Thé noir</h3></a>
+		<a href="#"><img src="img/tea/2.jpg" alt="Thé vert" title="Thé vert"><h3>Thé vert</h3></a>
+		<a href="#"><img src="img/tea/3.jpg" alt="Oolong" title="Oolong"><h3>Oolong</h3></a>
+		<a href="#"><img src="img/tea/4.jpg" alt="Thé blanc" title="Thé blanc"><h3>Thé blanc</h3></a>
+		<a href="#"><img src="img/tea/5.jpg" alt="Rooibos" title="Rooibos"><h3>Rooibos</h3></a>
 	</section>
 	<section id="section3articles">
 		<article>
 			<h2>Notre nouveauté</h2>
-			<img src="img/product/product1.jpg" alt="">
+			<img src="img/product/product1.jpg" alt="product1" title="product1">
 			<h3>Thé du hammam</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In est inventore facere aperiam distinctio, quas, mollitia ut odit architecto cum aliquam, explicabo omnis sunt vel dolorum accusamus! Ipsum, aut enim.</p>
 			<p>A partir de</p><strong class="prix">8,50€</strong>
@@ -34,7 +34,7 @@
 		</article>
 		<article>
 			<h2>Notre best-seller</h2>
-			<img src="img/product/product2.jpg" alt="">
+			<img src="img/product/product2.jpg" alt="product2" title="product2">
 			<h3>Thé du hammam</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In est inventore facere aperiam distinctio, quas, mollitia ut odit architecto cum aliquam, explicabo omnis sunt vel dolorum accusamus! Ipsum, aut enim.</p>
 			<p>A partir de</p><strong class="prix">8,50€</strong>
@@ -42,7 +42,7 @@
 		</article>
 		<article>
 			<h2>Notre coup de coeur</h2>
-			<img src="img/product/product3.jpg" alt="">
+			<img src="img/product/product3.jpg" alt="product3" title="product3">
 			<h3>Thé du hammam</h3>
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. In est inventore facere aperiam distinctio, quas, mollitia ut odit architecto cum aliquam, explicabo omnis sunt vel dolorum accusamus! Ipsum, aut enim.</p>
 			<p>A partir de</p><strong class="prix">8,50€</strong>
