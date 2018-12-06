ef8d5761a6bf301a1beb9f50b6ac02555c40595b
JeanLi06 <jliautaud@free.fr>
2018-12-06 18:17:42 +0100

    First commit

---
+++ footer.php
@@ -0,0 +1,47 @@
+<footer>
+	<nav>
+		<article><i class="fas fa-lock"></i><small>paiement sécurisé</small></article>
+		<article><i class="fas fa-truck"></i><small>livraison offerte</small></article>
+		<article><i class="fab fa-cc-mastercard"></i><small>carte de fidélité</small></article>
+		<article><i class="fas fa-phone"></i><small>section client</small></article>
+		<article><i class="fas fa-check"></i><small>garantie qualité</small></article>
+	</nav>
+	<section id="footerTroisArticles">
+		<article>
+		<h2>Cup of Tea</h2>
+		<ul>
+			<li>Lorem ipsum dolor.</li>
+			<li>Lorem ipsum dolor sit amet.</li>
+			<li>Lorem ipsum dolor</li>
+		</ul>
+		</article>
+		<article>
+		<h2>Commandez en ligne</h2>
+		<ul>
+			<li>Lorem ipsum dolor</li>
+			<li>Lorem ipsum  sit amet.</li>
+			<li>Lorem ipsum dolor sit amet.</li>
+			<li>Lorem ipsum dolor</li>
+		</ul>
+		</article>
+		<article>
+		<h2>Suivez-nous !</h2>
+		<ul>
+			<li>Lorem ipsum dolor sit amet.</li>
+			<li>Lorem ipsum dolor</li>
+			<li>Lorem  dolor</li>
+			<li>Lorem ipsum dolor</li>
+			<li>Lorem ipsum dolor sit amet.</li>
+		</ul>
+		</article>
+	</section>
+	<small id="licence">
+	    <a rel="license" href="https://3wa.fr/propriete-materiel-pedagogique/"><img alt="Propriété de la 3W Academy" style="border-width:0" src="https://3wa.fr/wp-content/themes/3wa2015/img/logos/big.png" /></a><br /><span>Cet exercice</span> de <a href="https://3wa.fr">3W Academy</a> est mis à disposition <a rel="license" href="https://3wa.fr/propriete-materiel-pedagogique/">pour l'usage personnel des étudiants, Pas de Rediffusion - Attribution - Pas d'Utilisation Commerciale - Pas de Modification - International</a>.<br />Les autorisations au-delà du champ de cette licence peuvent être obtenues auprès de <a href="mailto:contact@3wa.fr" rel="cc:morePermissions">contact@3wa.fr</a>. Les maquettes ont été réalisées par <a href="http://www.justine-muller.fr">Justine Muller</a>.
+	</small>
+	<div id='scroll_to_top' class='opacity'>
+		<a href="#"><i class="fa fa-angle-up"></i></a>
+	</div>
+</footer>
+
+</body>
+</html>
