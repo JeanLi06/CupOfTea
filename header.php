572b361ccf46a5e954962989fd2db64a0aa1035d
Jean Liautaud <jliautaud@free.fr>
2019-10-16 16:59:03 +0200

    léger refactoring

--- header.php
+++ header.php
@@ -1,39 +1,40 @@
 <!DOCTYPE html>
 <html lang="fr">
-	<head>
-		<meta charset="utf-8" />
-		<title>Cup of tea - Exercice Formation 3W Academy</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta name="Description"
-              content="Exercice réalisé lors de la formation 3W Academy. Fait partie du Portfolio de Jean Liautaud">
-        <link href="https://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-        <link rel="stylesheet" href="css/normalize.css">
-		<link rel="stylesheet" href="css/flexslider.css" type="text/css">
-		<script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
-		<script src="js/jquery.flexslider.js"></script>
-		<link rel="stylesheet" href="css/style.css">
-		<!-- SLIDER -->
-	</head>
-	<body>
-		<!--HEADER-->
-		<header>
-			<img src="img/ribbon.svg" alt="ribbon">
-			<section id="sectionHeader1">
-				<p>Livraison offerte à partir de 65€ d'achat</p>
-			</section>
-			<div class="enTete">
-				<section id="sectionHeader2">
-					<a href="index.php"><img src="img/logo.png" alt="logo" id="logo"></a>
-					<section id="sectionHeader3"><span id="panier">Mon Panier </span><span><i class="fas fa-shopping-cart"></i> 42,00€</span>
-					</section>
-			</section>
-			<nav>
-				<a href="thes.php">thés</a>
-				<a href="">grands crus</a>
-				<a href="">accessoires</a>
-				<a href="">épicerie</a>
-				<a href="about.php">notre histoire</a>
-			</nav>
-		</div>
-	</header>
+<head>
+    <meta charset="utf-8"/>
+    <title>Cup of tea - Exercice Formation 3W Academy</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Description"
+          content="Exercice réalisé lors de la formation 3W Academy. Fait partie du Portfolio de Jean Liautaud">
+    <link href="https://fonts.googleapis.com/css?family=Amaranth" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css"
+          integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="css/flexslider.css" type="text/css">
+    <script src="//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js"></script>
+    <script src="js/jquery.flexslider.js"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <!-- SLIDER -->
+</head>
+<body>
+<!--HEADER-->
+<header>
+    <img src="img/ribbon.svg" alt="ribbon">
+    <section id="sectionHeader1">
+        <p>Livraison offerte à partir de 65€ d'achat</p>
+    </section>
+    <div class="enTete">
+        <section id="sectionHeader2">
+            <a href="index.php"><img src="img/logo.png" alt="logo" id="logo"></a>
+            <div id="panier"><span>Mon Panier </span><span><i class="fas fa-shopping-cart"></i> 42,00€</span>
+            </div>
+        </section>
+        <nav>
+            <a href="thes.php">thés</a>
+            <a href="">grands crus</a>
+            <a href="">accessoires</a>
+            <a href="">épicerie</a>
+            <a href="about.php">notre histoire</a>
+        </nav>
+    </div>
+</header>
--- product.php
+++ product.php
@@ -1,47 +1,47 @@
 <?php include 'header.php'; ?>
     <main class="container product">
-        <section class="article">
-            <article>
-                <div class="two-columns">
-                    <div>
-                        <h3>Blue of London</h3>
-                        <h4>Thé noir à la bergamote</h4>
-                        <p>Réf: 133742</p>
-                        <img src="img/product/product1.jpg" alt="Blue of London">
+        <article class="article">
+            <div class="two-columns">
+                <div>
+                    <h3>Blue of London</h3>
+                    <h4>Thé noir à la bergamote</h4>
+                    <p>Réf: 133742</p>
+                    <img src="img/product/product1.jpg" alt="Blue of London">
+                </div>
+                <div>
+                    <div class="note">
+                        <i class="fas fa-star "></i>
+                        <i class="fas fa-star "></i>
+                        <i class="fas fa-star "></i>
+                        <i class="fas fa-star "></i>
+                        <i class="fas fa-star "></i>
+                        <label for="pochette">Voir les 56 avis Clients</label>
                     </div>
                     <div>
-                        <div class="note">
-                            <i class="fas fa-star "></i>
-                            <i class="fas fa-star "></i>
-                            <i class="fas fa-star "></i>
-                            <i class="fas fa-star "></i>
-                            <i class="fas fa-star "></i>
-                            <label for="pochette">Voir les 56 avis Clients</label>
-                        </div>
-                        <div>
-                            <select name="pochette" id="pochette">
-                                <option value="">Pochette de 100g</option>
-                                <option value="">Pochette de 250g</option>
-                                <option value="">Pochette de 500g</option>
-                            </select>
-                            <p class="prix">8,50€</p>
-                            <a href="" class="bouton">Ajouter au panier</a>
-                            <a href=""><i class="fas fa-heart"></i> Ajouter à ma liste d'envie</a>
-                        </div>
+                        <select name="pochette" id="pochette">
+                            <option value="">Pochette de 100g</option>
+                            <option value="">Pochette de 250g</option>
+                            <option value="">Pochette de 500g</option>
+                        </select>
+                        <p class="prix">8,50€</p>
+                        <a href="" class="bouton">Ajouter au panier</a>
+                        <a href=""><i class="fas fa-heart"></i> Ajouter à ma liste d'envie</a>
                     </div>
                 </div>
-                <section><p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cum dolorum fugit magnam
-                        odio. Delectus deleniti eum laborum minus nostrum quia sequi suscipit velit! At consectetur
-                        delectus non possimus reiciendis. Fugiat iure, natus. Accusamus, dolore dolores doloribus earum
-                        eveniet impedit ipsum iure labore laudantium magnam maxime mollitia nulla tempore temporibus ut
-                        veniam, voluptas. Doloremque ducimus ipsam, laudantium placeat quisquam reprehenderit?</p>
-                    <p>Delectus deleniti eum laborum minus nostrum quia sequi suscipit velit! At consectetur delectus
-                        non possimus reiciendis. Fugiat iure, natus. Accusamus, dolore dolores doloribus earum eveniet
-                        impedit ipsum iure labore laudantium magnam maxime mollitia nulla tempore temporibus ut veniam,
-                        voluptas. Doloremque ducimus ipsam, laudantium placeat quisquam reprehenderit?</p>
-                    <h4>Profitez d'une remise de 5% sur la pochette de 500g (prix déjà remisé)</h4>
-                    <h4>Profitez d'une remise de 10% sur le lot de 2 pochettes de 500g (prix déjà remisé)</h4>
-                </section>
-            </article>
+            </div>
+            <div>
+                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi cum dolorum fugit magnam
+                    odio. Delectus deleniti eum laborum minus nostrum quia sequi suscipit velit! At consectetur
+                    delectus non possimus reiciendis. Fugiat iure, natus. Accusamus, dolore dolores doloribus earum
+                    eveniet impedit ipsum iure labore laudantium magnam maxime mollitia nulla tempore temporibus ut
+                    veniam, voluptas. Doloremque ducimus ipsam, laudantium placeat quisquam reprehenderit?</p>
+                <p>Delectus deleniti eum laborum minus nostrum quia sequi suscipit velit! At consectetur delectus
+                    non possimus reiciendis. Fugiat iure, natus. Accusamus, dolore dolores doloribus earum eveniet
+                    impedit ipsum iure labore laudantium magnam maxime mollitia nulla tempore temporibus ut veniam,
+                    voluptas. Doloremque ducimus ipsam, laudantium placeat quisquam reprehenderit?</p>
+                <h4>Profitez d'une remise de 5% sur la pochette de 500g (prix déjà remisé)</h4>
+                <h4>Profitez d'une remise de 10% sur le lot de 2 pochettes de 500g (prix déjà remisé)</h4>
+            </div>
+        </article>
     </main>
 <?php include 'footer.php'; ?>
