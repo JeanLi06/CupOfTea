f03de8cce1c4af765a41951671e5cbcf7a8b02a7
Jean Liautaud <jliautaud@free.fr>
2019-10-17 10:23:05 +0200

    ajout images

--- thes.php
+++ thes.php
@@ -9,19 +9,19 @@
 		<section class = "listingThes3thes">
 			<article>
 				<h3>Blue of London</h3>
-				<img src="img/product/product1.jpg" alt="Blue of London">
+				<img src="img/product/product3_big.jpg" alt="Blue of London">
 				<p>A partir de</p><strong class="prix">8,50€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
 			<article>
 				<h3>Thé des Lords</h3>
-				<img src="img/product/product2.jpg" alt="Thé des Lords">
+				<img src="img/product/product6_big.jpg" alt="Thé des Lords">
 				<p>A partir de</p><strong class="prix">8,50€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
 			<article>
 				<h3>Thé des vahinés</h3>
-				<img src="img/product/product3.jpg" alt="Thé des vahinés">
+				<img src="img/product/product7_big.jpg" alt="Thé des vahinés">
 				<p>A partir de</p><strong class="prix">8,50€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
@@ -37,19 +37,19 @@
 		<section class = "listingThes3thes">
 			<article>
 				<h3>Thé du hamman</h3>
-				<img src="img/product/product1.jpg" alt="Vive les fêtes">
+				<img src="img/product/product1_big.jpg" alt="Vive les fêtes">
 				<p>A partir de</p><strong class="prix">8,40€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
 			<article>
 				<h3>Vive le thé !</h3>
-				<img src="img/product/product2.jpg" alt="Fleur d'orange Olong">
+				<img src="img/product/product4_big.jpg" alt="Fleur d'orange Olong">
 				<p>A partir de</p><strong class="prix">6,80€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
 			<article>
 				<h3>Thé des alizés</h3>
-				<img src="img/product/product3.jpg" alt="Olong 7 agrumes">
+				<img src="img/product/product5_big.jpg" alt="Olong 7 agrumes">
 				<p>A partir de</p><strong class="prix">9,30€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
@@ -65,19 +65,19 @@
         <section class = "listingThes3thes">
             <article>
                 <h3>Vive les fêtes</h3>
-                <img src="img/product/product1.jpg" alt="Vive les fêtes">
+                <img src="img/product/product8_big.jpg" alt="Vive les fêtes">
                 <p>A partir de</p><strong class="prix">11,10€</strong>
                 <a href="product.php">voir ce produit</a>
             </article>
             <article>
                 <h3>Fleur d'orange Olong</h3>
-                <img src="img/product/product2.jpg" alt="Fleur d'orange Olong">
+                <img src="img/product/product9_big.jpg" alt="Fleur d'orange Olong">
                 <p>A partir de</p><strong class="prix">10,90€</strong>
                 <a href="product.php">voir ce produit</a>
             </article>
             <article>
                 <h3>Olong 7 agrumes</h3>
-                <img src="img/product/product3.jpg" alt="Olong 7 agrumes">
+                <img src="img/product/product10_big.jpg" alt="Olong 7 agrumes">
                 <p>A partir de</p><strong class="prix">13,20€</strong>
                 <a href="product.php">voir ce produit</a>
             </article>
@@ -93,19 +93,19 @@
 		<section class = "listingThes3thes">
 			<article>
 				<h3>Thé des songes blancs</h3>
-				<img src="img/product/product1.jpg" alt="Blue of London">
+				<img src="img/product/product11_big.jpg" alt="Blue of London">
 				<p>A partir de</p><strong class="prix">12,00€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
 			<article>
 				<h3>Bai mu dan</h3>
-				<img src="img/product/product2.jpg" alt="Thé des Lords">
+				<img src="img/product/product12_big.jpg" alt="Thé des Lords">
 				<p>A partir de</p><strong class="prix">9,50€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
 			<article>
 				<h3>Aiguilles d'argent</h3>
-				<img src="img/product/product3.jpg" alt="Thé des vahinés">
+				<img src="img/product/product13_big.jpg" alt="Thé des vahinés">
 				<p>A partir de</p><strong class="prix">47,20€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
@@ -121,19 +121,19 @@
 		<section class = "listingThes3thes">
 			<article>
 				<h3>Rooibos à la verveine</h3>
-				<img src="img/product/product1.jpg" alt="Blue of London">
+				<img src="img/product/product14_big.jpg" alt="Blue of London">
 				<p>A partir de</p><strong class="prix">8,50€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
 			<article>
 				<h3>Spicy Passion</h3>
-				<img src="img/product/product2.jpg" alt="Thé des Lords">
+				<img src="img/product/product15_big.jpg" alt="Thé des Lords">
 				<p>A partir de</p><strong class="prix">8,50€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
 			<article>
 				<h3>Rooibos des amants</h3>
-				<img src="img/product/product3.jpg" alt="Thé des vahinés">
+				<img src="img/product/product16_big.jpg" alt="Thé des vahinés">
 				<p>A partir de</p><strong class="prix">8,50€</strong>
 				<a href="product.php">voir ce produit</a>
 			</article>
